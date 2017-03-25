468e17abf96177f987399483cbec978ed31b9d4c
Joshua Dean <joshua.dean@student.cs.manchester.ac.uk>
2017-03-25 22:14:47 +0000

    Made a new dir, want to make a website quick

--- nextYouTube/nextYouTube/urls.py
+++ nextYouTube/nextYouTube/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^temp/', include('temp.urls')),
+    url(r'^admin/', admin.site.urls)
 ]
